00af167ebc18e54a3120978b461ba2a2b3266d6a
Bharath Divyang <bharath.divyang@gmail.com>
2013-07-20 17:35:32 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alphachezzles::Application.config.secret_token = '59b5b9b29ee20beb9fe06ccca3adc689004fdbe029b11c72f2a48c3d2818b0436abcfd58e3ef0c08a545623018535ae3526effef646aaa8b51b3e9520840cf52'
